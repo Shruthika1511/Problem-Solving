2cdb47d81024572a814bd5ba29c06fbcb2195865
B SHRUTHIKA <112743361+Shruthika1511@users.noreply.github.com>
2024-08-05 11:20:41 +0530

    Add files via upload

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -1,2 +1,27 @@
+import java.util.*;
 public class PrimeFactors {
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        while(n%2==0)
+        {
+            System.out.print("2");
+            n=n/2;
+        }
+        for(int i=3;i*i<=n;i=i+2)
+        {
+            while(n%i==0)
+            {
+                System.out.print(i);
+                n=n/i;
+            }
+        }
+        if(n>2)
+        {
+            System.out.print(n);
+        }
+    }
+
+
 }
